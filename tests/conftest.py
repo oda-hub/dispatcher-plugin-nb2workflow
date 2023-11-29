c5ea3876738c4f1e0c17d44cb490c3ae2e1a47dc
burnout87 <gabriele.barni@gmail.com>
2023-11-29 18:52:25 +0100

    setting path json files

--- tests/conftest.py
+++ tests/conftest.py
@@ -24,15 +24,16 @@ def httpserver_listen_address():
 
 @pytest.fixture
 def mock_backend(httpserver):
-    with open('tests/responses/options.json', 'r') as fd:
+    responses_path = os.path.join(os.path.dirname(__file__), 'responses')
+    with open(os.path.join(responses_path, 'options.json'), 'r') as fd:
         respjson = json.loads(fd.read())
-    with open('tests/responses/lightcurve.json', 'r') as fd:
+    with open(os.path.join(responses_path, 'lightcurve.json'), 'r') as fd:
         runjson = json.loads(fd.read())
-    with open('tests/responses/table.json', 'r') as fd:
+    with open(os.path.join(responses_path, 'table.json'), 'r') as fd:
         table_json = json.loads(fd.read())
-    with open('tests/responses/ascii_binary.json', 'r') as fd:
+    with open(os.path.join(responses_path, 'ascii_binary.json'), 'r') as fd:
         bin_json = json.loads(fd.read())
-    with open('tests/responses/image.json', 'r') as fd:
+    with open(os.path.join(responses_path, 'image.json'), 'r') as fd:
         image_json = json.loads(fd.read())
         
     httpserver.expect_request('/').respond_with_data('')    
