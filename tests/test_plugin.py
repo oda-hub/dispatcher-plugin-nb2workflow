7ae7a05ee6c0fb8fd7f056f3ca1405d330ff73f2
burnout87 <gabriele.barni@gmail.com>
2024-03-28 09:27:37 +0100

    adapted test

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -511,11 +511,12 @@ def test_failed_nbhtml_download(live_nb2service,
 
 
 @pytest.mark.fullstack
+@pytest.mark.parametrize("public", [True, False])
 def test_file_download(live_nb2service,
                        conf_file,
                        dispatcher_live_fixture_with_external_products_url,
                        dispatcher_test_conf_with_external_products_url,
-                       caplog):
+                       public):
     with open(conf_file, 'r') as fd:
         conf_bk = fd.read()
 
@@ -535,6 +536,16 @@ def test_file_download(live_nb2service,
                   'query_type': 'Real',
                   'product_type': 'file_download'}
 
+        if not public:
+            token_payload_update = {
+                **token_payload,
+                "mstout": False,
+                "mssub": False,
+                "msdone": False,
+            }
+            encoded_token = jwt.encode(token_payload_update, secret_key, algorithm='HS256')
+            params['token'] = encoded_token
+
         p_file_path = DispatcherJobState.create_p_value_file(p_value=5)
         for i in range(5):
             list_file = open(p_file_path)
@@ -549,12 +560,19 @@ def test_file_download(live_nb2service,
 
         file_hash = make_hash_file(p_file_path)
 
-        dpars = urlencode(dict(file_list=f'dummy_file_{file_hash}',
-                               return_archive=False))
+        params_url = dict(file_list=f'dummy_file_{file_hash}',
+                          return_archive=False)
+        if not public:
+            params_url['token'] = encoded_token
+
+        dpars = urlencode(params_url)
         download_url_host = os.path.join(dispatcher_test_conf_with_external_products_url["products_url"], "dispatch-data/download_file")
         download_url_host_no_scheme = urlparse(download_url_host).path
         assert f'Max retries exceeded with url: {download_url_host_no_scheme}?{dpars}' in jdata['exit_status']['message']
-        assert f'{download_url_host}?{dpars}&token=INSERT_YOUR_TOKEN_HERE' == jdata['products']['analysis_parameters']['dummy_file']
+        dummy_file_url = f'{download_url_host}?{dpars}'
+        if not public:
+            dummy_file_url = dummy_file_url.replace(f"&token={encoded_token}", '')
+        assert f'{dummy_file_url}&token=INSERT_YOUR_TOKEN_HERE' == jdata['products']['analysis_parameters']['dummy_file']
 
     finally:
         with open(conf_file, 'w') as fd:
