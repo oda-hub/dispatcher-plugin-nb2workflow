22e68e21306fe63ad9d2f3bd20d3e538ccb7bca3
burnout87 <gabriele.barni@gmail.com>
2024-05-30 17:01:31 +0200

    testing with development branches

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -522,6 +522,78 @@ def test_failed_nbhtml_download(live_nb2service,
 
 @pytest.mark.fullstack
 @pytest.mark.parametrize("public", [True, False])
+def test_file_download_with_default_route_products_url_fn(live_nb2service,
+                                                          conf_file,
+                                                          dispatcher_live_fixture_with_default_route_products_url,
+                                                          dispatcher_test_conf_with_default_route_products_url,
+                                                          public):
+    with open(conf_file, 'r') as fd:
+        conf_bk = fd.read()
+
+    try:
+        with open(conf_file, 'w') as fd:
+            fd.write(config_real_nb2service % live_nb2service)
+
+        server = dispatcher_live_fixture_with_default_route_products_url
+        logger.info("constructed server: %s", server)
+
+        # ensure new conf file is read
+        c = requests.get(server + "/reload-plugin/dispatcher_plugin_nb2workflow")
+        assert c.status_code == 200
+
+        params = {'instrument': 'example',
+                  'query_status': 'new',
+                  'query_type': 'Real',
+                  'product_type': 'file_download'}
+
+        if not public:
+            token_payload_update = {
+                **token_payload,
+                "mstout": False,
+                "mssub": False,
+                "msdone": False,
+            }
+            encoded_token = jwt.encode(token_payload_update, secret_key, algorithm='HS256')
+            params['token'] = encoded_token
+
+        p_file_path = DispatcherJobState.create_p_value_file(p_value=5)
+        for i in range(5):
+            list_file = open(p_file_path)
+            c = requests.post(os.path.join(server, "run_analysis"),
+                              params=params,
+                              files={'dummy_file': list_file.read()})
+            list_file.close()
+            jdata = c.json()
+            print('test_download_file run_analysis jdata: ', json.dumps(jdata, indent=4, sort_keys=True))
+            assert c.status_code == 200
+            time.sleep(5)
+
+        file_hash = make_hash_file(p_file_path)
+
+        download_products_params_url = dict(file_list=f'{file_hash}',
+                                            _is_mmoda_url=True,
+                                            return_archive=False,
+                                            )
+
+        dpars = urlencode(download_products_params_url)
+        download_url_host = os.path.join(dispatcher_test_conf_with_default_route_products_url["products_url"], "dispatch-data/download_file")
+        download_url_host_no_scheme = urlparse(download_url_host).path
+        assert f'Max retries exceeded with url: {download_url_host_no_scheme}?{dpars}' in jdata['exit_status']['message']
+        download_file_params_url = dict(file_list=f'{file_hash}',
+                                        _is_mmoda_url=True,
+                                        return_archive=False,
+                                        )
+        dpars = urlencode(download_file_params_url)
+        dummy_file_url = f'{download_url_host}?{dpars}'
+        assert dummy_file_url == jdata['products']['analysis_parameters']['dummy_file']
+
+    finally:
+        with open(conf_file, 'w') as fd:
+            fd.write(conf_bk)
+
+
+@pytest.mark.fullstack
+@pytest.mark.parametrize("public", [True, False])
 def test_file_download(live_nb2service,
                        conf_file,
                        dispatcher_live_fixture_with_external_products_url,
@@ -570,13 +642,20 @@ def test_file_download(live_nb2service,
 
         file_hash = make_hash_file(p_file_path)
 
-        params_url = dict(file_list=f'{file_hash}',
-                          return_archive=False)
+        download_products_params_url = dict(file_list=f'{file_hash}',
+                                            _is_mmoda_url=True,
+                                            return_archive=False,
+                                            )
 
-        dpars = urlencode(params_url)
+        dpars = urlencode(download_products_params_url)
         download_url_host = os.path.join(dispatcher_test_conf_with_external_products_url["products_url"], "dispatch-data/download_file")
         download_url_host_no_scheme = urlparse(download_url_host).path
-        assert f'Max retries exceeded with url: {download_url_host_no_scheme}?{dpars}' in jdata['exit_status']['message']
+        assert f'An issue occurred when attempting to getting the file size at the url {download_url_host}?{dpars}' in jdata['exit_status']['message']
+        download_file_params_url = dict(file_list=f'{file_hash}',
+                                        _is_mmoda_url=True,
+                                        return_archive=False,
+                                        )
+        dpars = urlencode(download_file_params_url)
         dummy_file_url = f'{download_url_host}?{dpars}'
         assert dummy_file_url == jdata['products']['analysis_parameters']['dummy_file']
 
