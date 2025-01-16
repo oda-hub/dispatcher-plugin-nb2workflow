60ce52ede06140d25c5d70a16703725981b41a1f
burnout87 <gabriele.barni@gmail.com>
2025-01-16 14:43:01 +0100

    easier code

--- dispatcher_plugin_nb2workflow/products.py
+++ dispatcher_plugin_nb2workflow/products.py
@@ -422,9 +422,9 @@ class NB2WImageProduct(NB2WNumpyDataProduct):
         # Note that if criteria are not met, it will end up with taking the last extension.
         # In the case a new instrument is added, a further adaptation might be needed.
         data_id = 0
-        if len(self.dispatcher_data_prod.data.data_unit) >= 1:
+        if len(self.dispatcher_data_prod.data.data_unit) > 1:
             for unit_id, unit in enumerate(self.dispatcher_data_prod.data.data_unit):
-                if unit.header.get('IMATYPE', '') == 'SIGNIFICANCE' and unit.header.get('XTENSION', '') == 'IMAGE':
+                if unit.data is not None and unit.header.get('IMATYPE', '') == 'SIGNIFICANCE' and unit.header.get('XTENSION', '') == 'IMAGE':
                     data_id = unit_id
                     break
 
